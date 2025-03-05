cd9cd9c4dc4e372d6f9b585cd90772054086f0ec
mbeliar <93191478+igninus@users.noreply.github.com>
2025-03-05 08:58:48 +0400

    feat: +  routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,46 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from 'const';
 import Main from 'pages/main/main';
+import Login from 'pages/login/login';
+import Favorites from 'pages/favorites/favorites';
+import NotFound from 'pages/not-found/not-found';
+import Offer from 'pages/offer/offer';
+import PrivateRoute from 'components/private-route/private-route';
 
 
 type AppProps = {
   placesCount: number;
 }
 
-function App({placesCount}: AppProps): JSX.Element {
+function App({placesCount}: AppProps) {
   return (
-    <Main placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Main placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route path={AppRoute.Offer}>
+          <Route path=':id' element={<Offer />} />
+        </Route>
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  Offer = '/offer',
+  Favorites = '/favorites'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
